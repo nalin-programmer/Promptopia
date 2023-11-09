92898c749775bc7f1e84320f455498aa12ae6e85
NALIN AGRAWAL <nalinagrawal333@gmail.com>
2023-11-09 14:22:23 +0530

    edit and delete prompts added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,44 +4,44 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-    providers:[
-        GoogleProvider({
-            clientId:process.env.GOOGLE_ID,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET
-        })
-    ],
-    callbacks:{
-        async session({session}){
-            try{
-                const sessionUser = await User.findOne({ email: session.user.email });
-                session.user.id = sessionUser._id.toString();
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      try {
+        const sessionUser = await User.findOne({ email: session.user.email });
+        session.user.id = sessionUser._id.toString();
 
-                return session;
-            }catch(error){
-                console.log("session error",error)
-            }        
-        },
-        async signIn({profile}){
-            try {
-                await connectToDB()
-    
-                const userExists = await User.findOne({email: profile.email})
-                
-                if(!userExists){
-                    await User.create({
-                        email: profile.email,
-                        username: profile.name.replace(" ","").toLowerCase(),
-                        image:  profile.picture
-                    })
-                }
+        return session;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-                return true
-            } catch (error) {
-                console.log("signIn error",error)
-                return false
-            }
+        const userExists = await User.findOne({ email: profile.email });
+
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
         }
-    }
-})
 
-export {handler as GET, handler as POST}
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,8 @@ import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
+  const { data: session } = useSession();
+  const pathName = usePathname();
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
@@ -50,8 +52,24 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-grayscale-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
